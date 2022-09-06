20f96771aefabc500db0cfd3bbce8f4186b10125
timon-k <>
2022-09-06 18:30:39 +0200

    Add car entity

---
+++ src/main/java/com/example/carmanagement/Car.java
@@ -0,0 +1,57 @@
+package com.example.carmanagement;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Car {
+
+    public enum Status {
+        AVAILABLE("available"),
+        IN_MAINTENANCE("in-maintenance"),
+        OUT_OF_SERVICE("out-of-service");
+        private String value;
+
+        Status(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        public static Status fromValue(String value) {
+            for (Status b : Status.values()) {
+                if (b.value.equals(value)) {
+                    return b;
+                }
+            }
+            throw new IllegalArgumentException("Unexpected value '" + value + "'");
+        }
+    }
+
+    private @Id @GeneratedValue Long id;
+    private String brand;
+    private String licensePlate;
+    private String manufacturer;
+    private String operationCity;
+    private Status status;
+
+    private @CreatedDate Instant createdAt;
+    private @LastModifiedDate Instant lastModifiedAt;
+}
---
+++ src/test/java/com/example/carmanagement/CarRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.carmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Transactional
+@SpringBootTest
+public class CarRepositoryTest {
+
+    @Autowired CarRepository repository;
+    @Autowired AuditingEntityListener listener;
+    @Autowired EntityManager entityManager;
+
+    String licensePlate = "L-CS8877E";
+
+    Car createNewCar() {
+        var car = new Car();
+        car.setManufacturer("Carcorp");
+        car.setBrand("Flexa");
+        car.setLicensePlate(licensePlate);
+        car.setOperationCity("Newtown");
+        car.setStatus(Car.Status.IN_MAINTENANCE);
+        return car;
+    }
+
+    @Test
+    void carCreationShouldWork() {
+        assertThat(ReflectionTestUtils.getField(listener, "handler")).isNotNull();
+
+        var car = repository.save(createNewCar());
+
+        assertEquals(licensePlate, car.getLicensePlate());
+        assertNotNull(car.getCreatedAt());
+        assertNotNull(car.getLastModifiedAt());
+        assertNotNull(car.getId());
+    }
+
+    @Test
+    void updatingCarShouldWork() throws Exception {
+        var existingCar = repository.save(createNewCar());
+
+        // Force the ORM to enter audit timestamps now
+        entityManager.flush();
+        // Let a minimum amount of time pass in order to enforce modified audit timestamps below
+        Thread.sleep(10);
+
+        var newStatus = Car.Status.AVAILABLE;
+        var previousLastModifiedAt = existingCar.getLastModifiedAt();
+        var previousCreatedAt = existingCar.getCreatedAt();
+        existingCar.setStatus(newStatus);
+        var updatedCar = repository.save(existingCar);
+
+        entityManager.flush(); // Force the ORM to enter audit timestamps now
+
+        assertEquals(newStatus, updatedCar.getStatus());
+        assertEquals(previousCreatedAt, updatedCar.getCreatedAt());
+        assertThat(previousLastModifiedAt).isBefore(updatedCar.getLastModifiedAt());
+    }
+}
